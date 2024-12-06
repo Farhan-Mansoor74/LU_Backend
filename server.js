9bda3f4a22ec76d1d79981782735de9f081f3b85
Farhan Mohammed Mansoor <farhan.mansoordxb@gmail.com>
2024-12-06 10:58:36 +0400

    Server file comments

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+// Main file
+//Importing modules required
 import express from 'express';
 import clubsRouter from './routes/getClubs.js';
 import ordersRouter from './routes/addOrder.js';
@@ -5,25 +7,32 @@ import updateRouter from './routes/updateClubs.js';
 import logger from './logger.js';
 
 import cors from 'cors';
-import path from 'path'; // Required to resolve paths
 
 const app = express();
+const PORT = 5500;
+
+// Enabling Cross-origin resource sharing
 app.use(cors({
     origin: '*',
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
+
+// Middleware to log HTTP requests sent
 app.use(logger);
+
+// Middleware to parse incoming JSON request bodies
 app.use(express.json());
 
+// Serves static files from public directory
 app.use(express.static('public'));
 
-const PORT = 5500;
-
+// Mounting routers to specific routes
 app.use('/clubs', clubsRouter);
 app.use('/orders', ordersRouter);
 app.use('/updateClub', updateRouter);
 
+// Starts the server
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
